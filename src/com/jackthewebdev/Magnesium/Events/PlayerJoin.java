ade43a67e26c01d6a004b2643e235968529288ea
JackTheWebDev <46900664+JackTheWebDev@users.noreply.github.com>
2021-05-21 23:39:36 -0400

    Now tracks users first, latest join

--- src/com/jackthewebdev/Magnesium/Events/PlayerJoin.java
+++ src/com/jackthewebdev/Magnesium/Events/PlayerJoin.java
@@ -1,30 +1,48 @@
 package com.jackthewebdev.Magnesium.Events;
 import com.jackthewebdev.Magnesium.Magnesium;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class PlayerJoin implements Listener {
     Magnesium plugin;
+
     final FileConfiguration config;
 
-    public PlayerJoin(Magnesium instance){
-        plugin = instance;
-         config = plugin.getConfig();
+    private File file;
+    private FileConfiguration conf;
 
+    public PlayerJoin(Magnesium instance) {
+        plugin = instance;
+        config = plugin.getConfig();
+        loadConfigFile();
+    }
+
+    public void loadConfigFile(){
+        file = new File(plugin.getDataFolder(),"users.yml");
+        conf = YamlConfiguration.loadConfiguration(file);
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerJoin(PlayerJoinEvent event) throws IOException {
         Player player = event.getPlayer();
         if(config.getBoolean("OnPlayerJoin.Enabled")) {
             player.sendMessage(config.getString("OnPlayerJoin.Message") + ", " + ChatColor.valueOf(config.getString("OnPlayerJoin.PlayerNameColor")) + player.getName() + ChatColor.WHITE);
         }
-        //TODO: check if this is the players first time joining, if so log it in the config file if not just update the latest join
+        if(!conf.contains("Users."+player.getUniqueId()+".firstjoin")) {
+                conf.set("Users." + player.getUniqueId() + ".firstjoin", System.currentTimeMillis());
+        }
+        conf.set("Users."+player.getUniqueId()+".latestjoin",System.currentTimeMillis());
+        conf.save(file);
     }
 }
